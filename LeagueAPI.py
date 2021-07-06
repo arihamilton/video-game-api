133b5142bf01e56b9346a62f7e793ddbcf1277f1
Dave Ng'ang'a <dn789@cairn.edu>
2021-07-06 15:57:50 +0000

    initial commit

---
+++ LeagueAPI.py
@@ -0,0 +1,6 @@
+# 1. Connecting to League the API 
+# 2. Storing whatever response in the appropriate data type: (array)
+# 3. Setting up a MariaDB
+# 4. Updating database with get request response
+# 5. Integrating a pandas/matpllotlib funciton to display our information.
+
---
+++ PokemonAPI.py
@@ -0,0 +1,14 @@
+# 1. Connect to the Pokemon API
+# 2. Create a function to get a list of pokemon with a specific type
+# 3. Create a function to get a list of pokemon by gender
+# 4. Create a function to get a list of pokemon by their abilities
+# 5. Create a function to get a list of pokemon by their moveset
+
+# 6. Let the user use the above functions to generate a team and put this information into a new table
+
+git init
+git remote add origin (url)
+git add LeagueAPI.py
+git add PokemonAPI.py
+git commit -m 'initial commit'
+git push
