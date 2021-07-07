5bb911f670a3309f981a95d837af576aba1e7955
Arianna Hamilton <ariannahamilton@gmail.com>
2021-07-07 19:42:42 +0000

    graph type combinations and stats

--- PokemonAPI.py
+++ PokemonAPI.py
@@ -1,6 +1,8 @@
 import requests
+import requests_cache
 import json
 import pandas as pd
+import plotly.graph_objects as go
 
 #response = requests.get("https://api.open-notify.org/astros.json")
 #https://pokeapi.co/api/v2/ability/{insert pokemon name/id}
@@ -72,18 +74,126 @@ def compare_poke_lists(list_1, list_2):
       complete_list.append(pokemon)
   return complete_list
 
+def valid_type_input(type):
+  if type in TYPES:
+    return True
+  return False
+
+def generate_suggestion(str, list):
+  for item in list:
+    if str in item:
+      response = input("Did you mean " + item + "? (yes/no) ")
+      if response == 'yes':
+        return item
+  return -1
+      
+def get_user_input():
+    valid = False
+    type_1 = ""
+    type_2 = ""
+    valid_type1 = False
+    valid_type2 = False
+    while not valid:
+      while not valid_type1:
+          type_1 = input("Enter Type 1: ")
+          if valid_type_input(type_1):
+              valid_type1 = True
+          else:
+              print("Invalid input.")
+              suggestion = generate_suggestion(type_1, TYPES)
+              if suggestion != -1:
+                  type_1 = suggestion
+                  valid_type1 = True
+      
+      while not valid_type2:
+          type_2 = input("Enter Type 2: ")
+          if valid_type_input(type_2):
+              valid_type2 = True
+              valid = True
+          else:
+              print("Invalid input.")
+              suggestion = generate_suggestion(type_2, TYPES)
+              if suggestion != -1:
+                  type_2 = suggestion
+                  valid_type2 = True
+                  valid = True
+  
+    return [type_1, type_2]
+  
+def get_pokemon_in_list(list):
+  poke_list = []
+  for pokemon in list:
+    url = pokemon['url']
+    r = requests.get(url)
+    response_code = r.status_code
+    if response_code == 200:
+        dict = r.json()
+        poke_id = dict['id']
+        poke_name = dict['name']
+        sprite = dict['sprites']['front_default']
+        stats = []
+        stat_dict = dict['stats']
+        for item in stat_dict:
+          stat_name = item['stat']['name']
+          stat_value = item['base_stat']
+          stats.append([stat_name, stat_value])
+
+    poke_list.append([poke_id, poke_name, sprite, stats])
+    
+  return poke_list
+    
 #a = get_by_ability('torrent')
 #a_list = get_pokes_from_json(a)
 #for pokes in a_list:
   #print(pokes)
+  
+TYPES = ['normal', 'fire', 'water', 'grass', 'electric', 'ice', 'fighting', 'poison', 'ground', 
+         'flying', 'psychic', 'bug', 'rock', 'ghost', 'dark', 'dragon', 'steel', 'fairy']
 
-t = get_by_type('fire')
+#requests_cache.install_cache('pokemon_cache', backend='sqlite', expire_after=3600)
+
+user_input = get_user_input()
+print(user_input)
+t = get_by_type(user_input[0])
 t_list = get_pokes_from_json(t)
 
-t2 = get_by_type('flying')
+t2 = get_by_type(user_input[1])
 t2_list = get_pokes_from_json(t2)
 
-print(compare_poke_lists(t_list, t2_list))
+combination = compare_poke_lists(t_list, t2_list)
+
+pokes = get_pokemon_in_list(combination)
+
+
+#fig = go.Figure(data=go.Bar(y=[3, 5, 1, 2, 4])) # create a figure
+poke = pokes[0]
+fig = go.Figure()
+
+stats = poke[3]
+x = []
+y = []
+for items in stats:
+  x.append(items[0])
+  y.append(items[1])
+
+fig.add_trace(go.Bar(x=x, y=y))
+
+# Add image
+img = poke[2]
+fig.add_layout_image(
+    dict(
+        source=img,
+        xref="paper", yref="paper",
+        x=1, y=1.05,
+        sizex=0.2, sizey=0.2,
+        xanchor="right", yanchor="bottom"
+    )
+)
+
+name = poke[1]
+# Update layout
+fig.update_layout(title=(name))
+fig.write_html('' + name + '.html') # export to HTML file
 
 
 #blah
