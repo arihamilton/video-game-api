825f426844dad632e637e60243d7bdc04883111e
Dave Ng'ang'a <dn789@cairn.edu>
2021-07-08 17:49:46 +0000

    league of legends api completed

---
+++ COD_API.py
@@ -0,0 +1,85 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+# 1. Connecting to COD the API 
+# GET 'https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/battle/gamer/' + username + '/profile/type/mp'
+
+
+import asyncio
+
+import callofduty
+from callofduty import Mode, Platform, Title
+
+
+async def main():
+    client = await callofduty.Login("evadaniam@gmail.com", "Mkawana1972")
+
+    results = await client.SearchPlayers(Platform.Activision, "Captain Price", limit=3)
+    for player in results:
+        print(f"{player.username} ({player.platform.name})")
+
+    me = results[1]
+    profile = await me.profile(Title.ModernWarfare, Mode.Multiplayer)
+
+    level = profile["level"]
+    kd = profile["lifetime"]["all"]["properties"]["kdRatio"]
+    wl = profile["lifetime"]["all"]["properties"]["wlRatio"]
+
+    print(f"\n{me.username} ({me.platform.name})")
+    print(f"Level: {level}, K/D Ratio: {kd}, W/L Ratio: {wl}")
+
+asyncio.get_event_loop().run_until_complete(main())
+
+
+def get_by_username():
+    username = input("Username: ")
+    password = input("Password: ")
+    final_token = ''
+    csrf_token = requests.get('https://profile.callofduty.com/cod/login')
+    csrf_parsed = BeautifulSoup(csrf_token.content, 'lxml')
+    token = csrf_parsed.find_all("input")
+    final_token = get_token(token)
+    #print(token)
+    data = {'username':  username, 
+            'password':  password, 
+            'remember_me': 'true', 
+            '_csrf': final_token
+           }
+    files=[]
+    headers = {
+      'Cookie': 'XSRF-TOKEN='+ final_token   
+    }
+    print("code reaches here before post request")
+    auth_response = requests.request("POST","https://profile.callofduty.com/do_login?new_SiteId=cod", headers=headers, data=data, files=files, verify=True, timeout=5)
+    print(auth_response.status_code)
+    print("code reaches here after post request")
+    #{'status': 'error', 'data': {'type': 'com.activision.mt.common.stdtools.exceptions.NoStackTraceException', 'message': 'Not permitted: not authenticated'}}
+    print(auth_response.status_code)
+    #r = requests.get('https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/battle/gamer/' + username + '/profile/type/mp', headers=auth_response)
+    #File = r.json()
+    #print(File)
+    #print(File)
+    #
+def get_token(token):
+    last_token =''
+    for thing in token:
+        if '_csrf' in str(thing):
+            print(thing)
+            x = (len(thing)-3)
+            string_thing = str(thing)
+            last_token = string_thing[41:x]
+        else:
+            break;
+    return last_token
+    # Get call for CSRF Token
+    # Post call to login 
+    # Get call for api
+
+# 2. Storing whatever response in the appropriate data type: (array)
+# 3. Setting up a MariaDB
+# 4. Updating database with get request response
+# 5. Integrating a pandas/matpllotlib funciton to display our information. information
+
+#get_by_username() # %23
+# username 'Lho%2311661'
+#<input name="_csrf" type="hidden" value="YcXZRXkdfrHFX4oF_GS-8WtWWbzB5LNaYAmTeKHSf4tef1gv26XjpY1yE1sDt-nx"/>
---
+++ LeagueOfLegendsAPI.py
@@ -0,0 +1,93 @@
+import requests
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+# Connect to the League API
+# Obtain information with a get requests
+# Place information into JSON file
+# Database that jawn 
+# Graphs and shit
+
+def init():
+    # get summoner region and name via input
+    # Maybe add a function to parse the response and ensure a correct region is collected
+    summoner_name = input("League Summoner Name: ")
+    #summoner_name = "LH0"
+    #summoner_region = "na1"
+    summoner_region = input("Your Region: ")
+    api_key = 'RGAPI-96982efc-3da2-4ecd-a157-6046211d8b92'
+    get_request_response = initial_get_request(summoner_name,summoner_region,api_key)
+    summoner_id = get_summoner_id(get_request_response)
+    graph(champion_mastery(summoner_name,summoner_region,summoner_id,api_key))
+    
+    
+def initial_get_request(_name,_region,_api):
+    url = "https://"+_region+".api.riotgames.com/lol/summoner/v4/summoners/by-name/"+_name+"?api_key="+_api
+
+    response = requests.request("GET", url)
+    return response
+    
+def get_summoner_id(response):
+  
+    File = response.json()
+    encrypted_summoner_id = File['id']
+    summoner_puuid = File['puuid']
+    return encrypted_summoner_id
+  
+def get_summoner_puuid(response):
+  
+    File = response.json()
+    summoner_puuid = File['puuid']
+    
+    return summoner_puuid
+    
+    
+def champion_mastery(_name,_region,encrypted_summoner_id,_api):
+  
+    url = "https://"+_region+".api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/"+ encrypted_summoner_id+"?api_key="+_api 
+    response = requests.request("GET", url)
+    return graph_champion_mastery(response)
+  
+def graph_champion_mastery(response):
+  
+    File = response.json()
+    # add a name column to our File
+    New_File = name_column(File)
+    data_frame = pd.DataFrame(New_File)
+    # no need for the summonerid in our file
+    data_frame = data_frame.drop(['summonerId'], axis=1)
+    
+    return data_frame
+        
+def graph(_frame_):
+  
+    plot = _frame_.iloc[0:10].plot.bar(x="ChampionName", 
+                                       y=["championPointsSinceLastLevel","championPoints","championLevel"],
+                                       subplots=True,figsize=(15,10))
+    #_frame_.plot.line(y="tokensEarned")
+  
+# A small problem I've noticed is that the original data set from riot games doesn't name the champions. Instead it only provides id's
+# Below is a function to convert champion add a column for champion names given ids to my data_frame
+def name_column(_data_):
+    # the source for my name json file
+    url = "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json"
+    champion_list = requests.request("GET", url)
+    File = champion_list.json()
+    _data_frame = File.pop('data')
+    names_list=[]
+    for key,value in _data_frame.items():
+        temp_dict = {}
+        element = _data_frame[key]
+        temp_dict['name'] = element['id'] 
+        temp_dict['id'] = element['key'] 
+        names_list.append(temp_dict)
+    
+    for thing in _data_:
+        for things in names_list:
+              if int(thing['championId']) == int(things['id']):
+                    thing['ChampionName'] = str(things['name'])
+    return _data_                 
+
+    
+init()
+plt.show()
