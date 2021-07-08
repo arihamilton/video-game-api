cd52428d94fad910299f1792932bb0fc2fc7909f
Dave Ng'ang'a <dn789@cairn.edu>
2021-07-08 17:50:58 +0000

    cod api not working

--- COD_API.py
+++ COD_API.py
@@ -4,33 +4,6 @@ import json
 # 1. Connecting to COD the API 
 # GET 'https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/battle/gamer/' + username + '/profile/type/mp'
 
-
-import asyncio
-
-import callofduty
-from callofduty import Mode, Platform, Title
-
-
-async def main():
-    client = await callofduty.Login("evadaniam@gmail.com", "Mkawana1972")
-
-    results = await client.SearchPlayers(Platform.Activision, "Captain Price", limit=3)
-    for player in results:
-        print(f"{player.username} ({player.platform.name})")
-
-    me = results[1]
-    profile = await me.profile(Title.ModernWarfare, Mode.Multiplayer)
-
-    level = profile["level"]
-    kd = profile["lifetime"]["all"]["properties"]["kdRatio"]
-    wl = profile["lifetime"]["all"]["properties"]["wlRatio"]
-
-    print(f"\n{me.username} ({me.platform.name})")
-    print(f"Level: {level}, K/D Ratio: {kd}, W/L Ratio: {wl}")
-
-asyncio.get_event_loop().run_until_complete(main())
-
-
 def get_by_username():
     username = input("Username: ")
     password = input("Password: ")
